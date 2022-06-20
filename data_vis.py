e99ac62a88bd9e6a42c9f7b09d14f45893df4c94
AdamPendry <55507747+AdamPendry@users.noreply.github.com>
2022-06-20 13:20:02 -0400

    converted to a flask webapp

--- data_vis.py
+++ data_vis.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # parse the .wav file into frequency and time arrays
-samplerate, data = read('./data/output.wav')
+samplerate, data = read('./data/CantinaBand3.wav')
 duration = len(data)/samplerate
-time = np.arange(0,duration,1/samplerate)
+time = np.arange(0, duration, 1.0/samplerate)
 
 # create the plot
-plt.plot(time,data)
+plt.plot(time, data)
 plt.xlabel('Time [s]')
 plt.ylabel('Amplitude')
-plt.title('output.wav')
+plt.title('CantinaBand3.wav')
 plt.show()
--- live_data_vis.py
+++ live_data_vis.py
@@ -1,6 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
+
 import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+matplotlib.use('TKAgg')
 
 fig, ax = plt.subplots()
 
@@ -11,13 +14,16 @@ x = np.arange(0, max_x)
 ax.set_ylim(0, max_rand)
 line, = ax.plot(x, np.random.randint(0, max_rand, max_x))
 
+
 def init():  # give a clean slate to start
     line.set_ydata([np.nan] * len(x))
     return line,
 
+
 def animate(i):  # update the y values (every 1000ms)
     line.set_ydata(np.random.randint(0, max_rand, max_x))
     return line,
+
 
 ani = animation.FuncAnimation(
     fig, animate, init_func=init, interval=1000, blit=True, save_count=10)
